0177a29b1372d61908211956dcbf04232ffe8442
solomon-m <solomon.mark@a-cti.com>
2017-01-20 14:30:24 +0530

    import and export in es6

---
+++ Es6/10-import and export/lib.js
@@ -0,0 +1,13 @@
+export function square(x) {
+    return x * x;
+}
+
+export function addTwo(a,b){
+	return a+b;
+}
+function subTwo(a,b){
+   //return a>b ? return a-b : `No a :${a} should greater than b: ${b}`;
+   return a - b;
+}
+
+export {subTwo}
