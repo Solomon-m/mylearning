7f4c27f394dd7a2faedbeb775a89a2a1859c4c25
solomon-m <solomon.mark@a-cti.com>
2017-02-12 15:38:57 +0530

    drag and drop using Observable

---
+++ Asynchronous programming-End of the loop/08-Dragdrop-with-Observables.js
@@ -0,0 +1,46 @@
+var Observable = Rx.Observable;
+
+// Observable dont store items in memory. 
+var parent = document.getElementById("parent");
+var widget = document.getElementById("widget");
+
+var mouseDowns = Observable.fromEvent(widget, "mousedown");
+var parentMouseMoves = Observable.fromEvent(parent, "mousemove");
+var parentMouseUps = Observable.fromEvent(parent, "mouseup");
+
+// var stocks = 
+//   exchanges.
+//     map(function(exchange) {
+//       return exchange.stocks.
+//         filter(function(stock) { return stock.price >= 100.00; });
+//     }).
+//     concatAll();
+
+var drags = 
+  mouseDowns.
+    map(function(e) {
+      return parentMouseMoves.
+        takeUntil(parentMouseUps);
+    }).
+    concatAll();
+
+
+ // drags.forEach(function(e){
+ //      widget.style.left = e.clientX + "px";
+ //      widget.style.top = e.clientY + "px";
+ //    });
+
+var subscription = 
+  drags.forEach(
+    function onNext(e) {
+      widget.style.left = e.clientX + "px";
+      widget.style.top = e.clientY + "px";
+    },
+    function onError(error) {
+      console.log('error');
+    },
+    function onCompleted() {
+      
+    });
+
+   
