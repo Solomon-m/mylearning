4d2f55bf56962d62c6c78855f9004b69476cf8bf
solomon-m <solomon.mark@a-cti.com>
2017-04-01 23:13:22 +0530

    few more examples

---
+++ Rxjs/01-Introduction to Rxjs/06-rendering on dom with refresh button.js
@@ -0,0 +1,62 @@
+var refreshButton = document.querySelector('.refresh');
+
+var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+
+var startupRequestStream = Rx.Observable.just('https://api.github.com/users');
+
+var requestOnRefreshStream = refreshClickStream
+  .map(ev => {
+    var randomOffset = Math.floor(Math.random()*500);
+    return 'https://api.github.com/users?since=' + randomOffset; // offset give some number from 0 - 500 with that we are triggering call. 
+  });
+
+var responseStream = startupRequestStream.merge(requestOnRefreshStream) // merging both refresh and request streams.. 
+  .flatMap(requestUrl =>
+    Rx.Observable.fromPromise(jQuery.getJSON(requestUrl))
+  );
+
+  // marble diagrams. 
+  // ------a---b----c-----
+  // s--------------------
+  //        merge
+  // s----a----b-----c----
+
+function createSuggestionStream(responseStream) {
+  return responseStream.map(listUser =>
+    listUser[Math.floor(Math.random()*listUser.length)]
+  )
+  .startWith(null)
+  .merge(refreshClickStream.map(ev => null));
+}
+
+var suggestion1Stream = createSuggestionStream(responseStream);
+var suggestion2Stream = createSuggestionStream(responseStream);
+var suggestion3Stream = createSuggestionStream(responseStream);
+
+// Rendering ---------------------------------------------------
+function renderSuggestion(suggestedUser, selector) {
+  var suggestionEl = document.querySelector(selector);
+  if (suggestedUser === null) {
+    suggestionEl.style.visibility = 'hidden';
+  } else {
+    suggestionEl.style.visibility = 'visible';
+    var usernameEl = suggestionEl.querySelector('.username');
+    usernameEl.href = suggestedUser.html_url;
+    usernameEl.textContent = suggestedUser.login;
+    var imgEl = suggestionEl.querySelector('img');
+    imgEl.src = "";
+    imgEl.src = suggestedUser.avatar_url;
+  }
+}
+
+suggestion1Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion1');
+});
+
+suggestion2Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion2');
+});
+
+suggestion3Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion3');
+});
