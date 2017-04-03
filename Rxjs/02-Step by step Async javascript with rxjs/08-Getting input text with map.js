3a7bd8520182512dff4e970d0be71b4df982924f
solomon-m <solomon.mark@a-cti.com>
2017-04-03 22:01:54 +0530

    few more examples in async with rxjs

---
+++ Rxjs/02-Step by step Async javascript with rxjs/08-Getting input text with map.js
@@ -0,0 +1,48 @@
+const Observable = Rx.Observable;
+
+const startButton = document.querySelector('#start');
+const halfButton = document.querySelector('#half');
+const quarterButton = document.querySelector('#quarter');
+
+const stopButton = document.querySelector('#stop');
+const resetButton = document.querySelector('#reset');
+
+
+const start$ = Observable.fromEvent(startButton, 'click');
+const half$ = Observable.fromEvent(halfButton, 'click');
+const quarter$ = Observable.fromEvent(quarterButton, 'click');
+
+const stop$ = Observable.fromEvent(stopButton, 'click');
+const reset$ = Observable.fromEvent(resetButton, 'click');
+
+
+const data = {count:0};
+const inc = (acc)=> ({count: acc.count + 1});
+const reset = (acc)=> data;
+
+const starters$ = Observable.merge(
+    start$.mapTo(1000),
+    half$.mapTo(500),
+    quarter$.mapTo(250)
+);
+
+const intervalActions = (time)=> Observable.merge(
+    Observable.interval(time)
+        .takeUntil(stop$).mapTo(inc),
+    reset$.mapTo(reset)
+);
+
+starters$
+    .switchMap(intervalActions)
+    .startWith(data)
+    .scan((acc, curr)=> curr(acc))
+    .subscribe((x)=> console.log(x));
+
+// Input text 
+const input = document.querySelector('#input');
+const input$ = Observable.fromEvent(input, 'input')
+  .map(event => event.target.value);
+
+input$
+  .subscribe((x)=> console.log(x));
+
